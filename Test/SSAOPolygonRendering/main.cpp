7a724356b4c1a777f223ea6a3a894cabf146c31d
Naohisa Sakamoto <naohisa.sakamoto@people.kobe-u.ac.jp>
2020-08-29 09:33:16 +0900

    Update main.cpp

--- Test/SSAOPolygonRendering/main.cpp
+++ Test/SSAOPolygonRendering/main.cpp
@@ -1,5 +1,5 @@
-#include <kvs/glut/Application>
-#include <kvs/glut/Screen>
+#include <kvs/Application>
+#include <kvs/Screen>
 #include <kvs/ShaderSource>
 #include <kvs/PolygonImporter>
 #include <kvs/PolygonRenderer>
@@ -8,14 +8,9 @@
 #include <kvs/ScreenCaptureEvent>
 #include <kvs/TargetChangeEvent>
 #include <kvs/KeyPressEventListener>
-#include <AmbientOcclusionRendering/Lib/PolygonToPolygon.h>
+#include <kvs/PolygonToPolygon>
 #include <AmbientOcclusionRendering/Lib/SSAOPolygonRenderer.h>
 
-namespace
-{
-float SamplingRadius = 0.5f;
-size_t SamplingPoints = 256;
-}
 
 namespace
 {
@@ -27,209 +22,139 @@ inline kvs::PolygonObject* ImportPolygonObject( const std::string filename )
     const size_t npolygons = polygon->numberOfConnections();
     if ( npolygons > 0 && nvertices != 3 * npolygons )
     {
-        kvs::PolygonObject* temp = new AmbientOcclusionRendering::PolygonToPolygon( polygon );
+        kvs::PolygonObject* temp = new kvs::PolygonToPolygon( polygon );
         delete polygon;
         polygon = temp;
         }
     return polygon;
 }
 
-inline kvs::RendererBase* CreateRenderer( const bool ssao )
+inline kvs::RendererBase* CreateRenderer( const bool ssao, const float radius, const size_t points )
 {
     if ( ssao )
     {
-        typedef AmbientOcclusionRendering::SSAOPolygonRenderer Renderer;
-        Renderer* renderer = new Renderer();
+        auto* renderer = new AmbientOcclusionRendering::SSAOPolygonRenderer();
         renderer->setName( "Renderer" );
-        renderer->setSamplingSphereRadius( ::SamplingRadius );
-        renderer->setNumberOfSamplingPoints( ::SamplingPoints );
+        renderer->setSamplingSphereRadius( radius );
+        renderer->setNumberOfSamplingPoints( points );
         renderer->enableShading();
         return renderer;
     }
     else
     {
-        typedef kvs::glsl::PolygonRenderer Renderer;
-        Renderer* renderer = new Renderer();
+        auto* renderer = new kvs::glsl::PolygonRenderer();
         renderer->setName( "Renderer" );
         renderer->enableShading();
         return renderer;
     }
 }
 
-}
+} // end of namespace
 
-class SSAOCheckBox : public kvs::CheckBox
-{
-private:
-    kvs::Scene* m_scene;
-
-public:
-    SSAOCheckBox( kvs::glut::Screen* screen ):
-        kvs::CheckBox( screen ),
-        m_scene( screen->scene() ) {}
-
-    void stateChanged()
-    {
-        if ( !state() )
-        {
-            typedef AmbientOcclusionRendering::SSAOPolygonRenderer Renderer;
-            const Renderer* renderer = Renderer::DownCast( m_scene->renderer( "Renderer" ) );
-            if ( renderer )
-            {
-                m_scene->replaceRenderer( "Renderer", ::CreateRenderer( false ) );
-            }
-        }
-        else
-        {
-            typedef kvs::glsl::PolygonRenderer Renderer;
-            const Renderer* renderer = Renderer::DownCast( m_scene->renderer( "Renderer" ) );
-            if ( renderer )
-            {
-                m_scene->replaceRenderer( "Renderer", ::CreateRenderer( true ) );
-            }
-        }
-    }
-};
-
-class SamplingRadiusSlider : public kvs::Slider
-{
-private:
-    kvs::Scene* m_scene;
-
-public:
-    SamplingRadiusSlider( kvs::glut::Screen* screen ):
-        kvs::Slider( screen ),
-        m_scene( screen->scene() ) {}
-
-    void sliderMoved()
-    {
-        ::SamplingRadius = current_value();
-        setCaption( "Radius: " + kvs::String::ToString( ::SamplingRadius ) );
-    }
-
-    void sliderReleased()
-    {
-        typedef AmbientOcclusionRendering::SSAOPolygonRenderer Renderer;
-        const bool ssao = ( Renderer::DownCast( m_scene->renderer( "Renderer" ) ) != NULL );
-        if ( ssao )
-        {
-            m_scene->replaceRenderer( "Renderer", ::CreateRenderer( true ) );
-        }
-    }
-
-private:
-    float current_value()
-    {
-        const float v = int( value() * 2 ) * 0.5;
-        return kvs::Math::Clamp( v, minValue(), maxValue() );
-    }
-};
-
-class SamplingPointsSlider : public kvs::Slider
-{
-private:
-    kvs::Scene* m_scene;
-
-public:
-    SamplingPointsSlider( kvs::glut::Screen* screen ):
-        kvs::Slider( screen ),
-        m_scene( screen->scene() ) {}
-
-    void sliderMoved()
-    {
-        ::SamplingPoints = int( value() );
-        setCaption( "Points: " + kvs::String::ToString( ::SamplingPoints ) );
-    }
-
-    void sliderReleased()
-    {
-        typedef AmbientOcclusionRendering::SSAOPolygonRenderer Renderer;
-        const bool ssao = ( Renderer::DownCast( m_scene->renderer( "Renderer" ) ) != NULL );
-        if ( ssao )
-        {
-            m_scene->replaceRenderer( "Renderer", ::CreateRenderer( true ) );
-        }
-    }
-};
-
-class KeyPressEvent : public kvs::KeyPressEventListener
-{
-private:
-    std::vector<kvs::WidgetBase*> m_widgets;
-
-public:
-    void addWidget( kvs::WidgetBase* widget )
-    {
-        m_widgets.push_back( widget );
-    }
-
-    void update( kvs::KeyEvent* event )
-    {
-        switch ( event->key() )
-        {
-        case kvs::Key::i:
-        {
-            for ( size_t i = 0; i < m_widgets.size(); i++ )
-            {
-                if ( m_widgets[i]->isShown() ) { m_widgets[i]->hide(); }
-                else { m_widgets[i]->show(); }
-            }
-            break;
-        }
-        default: break;
-        }
-    }
-};
 
 int main( int argc, char** argv )
 {
     kvs::ShaderSource::AddSearchPath("../../Lib");
 
     // Application and screen.
-    kvs::glut::Application app( argc, argv );
-    kvs::glut::Screen screen( &app );
+    kvs::Application app( argc, argv );
+    kvs::Screen screen( &app );
     screen.setTitle( "Screen Space Ambient Occlusion" );
     screen.setBackgroundColor( kvs::RGBColor::White() );
     screen.show();
 
-    // Input variables.
-    const std::string filename = argv[1];
-    const bool ssao = true;
+    // Parameters.
+    bool ssao = true;
+    float radius = 0.5f;
+    size_t points = 256;
 
     // Visualization pipeline.
-    kvs::PolygonObject* object = ::ImportPolygonObject( filename );
-    screen.registerObject( object, ::CreateRenderer( ssao ) );
+    const std::string filename = argv[1];
+    auto* object = ::ImportPolygonObject( filename );
+    auto* renderer = ::CreateRenderer( ssao, radius, points );
+    screen.registerObject( object, renderer );
 
     // Widgets.
-    SSAOCheckBox ssao_check_box( &screen );
+    kvs::CheckBox ssao_check_box( &screen );
     ssao_check_box.setCaption( "SSAO" );
     ssao_check_box.setState( ssao );
     ssao_check_box.setMargin( 10 );
+    ssao_check_box.stateChanged(
+        [&] ()
+        {
+            ssao = ssao_check_box.state();
+            renderer = ::CreateRenderer( ssao, radius, points );
+            screen.scene()->replaceRenderer( "Renderer", renderer );
+        } );
     ssao_check_box.show();
 
-    const float radius = ::SamplingRadius;
-    SamplingRadiusSlider radius_slider( &screen );
-    radius_slider.setCaption( "Radius: " + kvs::String::ToString( radius ) );
+    kvs::Slider radius_slider( &screen );
+    radius_slider.setCaption( "Radius: " + kvs::String::From( radius ) );
     radius_slider.setValue( radius );
     radius_slider.setRange( 0.1, 5.0 );
     radius_slider.setMargin( 10 );
-    radius_slider.setPosition( ssao_check_box.x(), ssao_check_box.y() + ssao_check_box.height() );
+    radius_slider.anchorToBottom( &ssao_check_box );
+    radius_slider.sliderMoved(
+        [&] ()
+        {
+            const float min_value = radius_slider.minValue();
+            const float max_value = radius_slider.maxValue();
+            const float v = int( radius_slider.value() * 2 ) * 0.5f;
+            radius = kvs::Math::Clamp( v, min_value, max_value );
+            radius_slider.setCaption( "Radius: " + kvs::String::From( radius ) );
+        } );
+    radius_slider.sliderReleased(
+        [&] ()
+        {
+            if ( ssao )
+            {
+                renderer = ::CreateRenderer( ssao, radius, points );
+                screen.scene()->replaceRenderer( "Renderer", renderer );
+            }
+        } );
     radius_slider.show();
 
-    const int npoints = ::SamplingPoints;
-    SamplingPointsSlider points_slider( &screen );
-    points_slider.setCaption( "Points: " + kvs::String::ToString( npoints ) );
-    points_slider.setValue( npoints );
+    kvs::Slider points_slider( &screen );
+    points_slider.setCaption( "Points: " + kvs::String::From( points ) );
+    points_slider.setValue( points );
     points_slider.setRange( 1, 256 );
     points_slider.setMargin( 10 );
-    points_slider.setPosition( radius_slider.x(), radius_slider.y() + radius_slider.height() - 10 );
+    points_slider.anchorToBottom( &radius_slider );
+    points_slider.sliderMoved(
+        [&] ()
+        {
+            points = int( points_slider.value() );
+            points_slider.setCaption( "Points: " + kvs::String::From( points ) );
+        } );
+    points_slider.sliderReleased(
+        [&] ()
+        {
+            if ( ssao )
+            {
+                renderer = ::CreateRenderer( ssao, radius, points );
+                screen.scene()->replaceRenderer( "Renderer", renderer );
+            }
+        } );
     points_slider.show();
 
     // Events.
-    KeyPressEvent key_event;
-    key_event.addWidget( &ssao_check_box );
-    key_event.addWidget( &radius_slider );
-    key_event.addWidget( &points_slider );
+    kvs::KeyPressEventListener key_event(
+        [&] ( kvs::KeyEvent* event )
+        {
+            switch ( event->key() )
+            {
+            case kvs::Key::i:
+            {
+                const bool visible = ssao_check_box.isVisible();
+                ssao_check_box.setVisible( !visible );
+                radius_slider.setVisible( !visible );
+                points_slider.setVisible( !visible );
+                screen.redraw();
+                break;
+            }
+            default: break;
+            }
+        } );
     screen.addEvent( &key_event );
 
     kvs::ScreenCaptureEvent capture_event;
