6ca5a93aeff19745ddb55638c0b699fd2de35901
Naohisa Sakamoto <naohisa.sakamoto@people.kobe-u.ac.jp>
2020-09-10 17:18:59 +0900

    Update SSAOPolygonRenderer.cpp

--- Lib/SSAOPolygonRenderer.cpp
+++ Lib/SSAOPolygonRenderer.cpp
@@ -37,7 +37,6 @@ void SSAOPolygonRenderer::exec( kvs::ObjectBase* object, kvs::Camera* camera, kv
     kvs::OpenGL::WithPushedAttrib p( GL_ALL_ATTRIB_BITS );
     kvs::OpenGL::Enable( GL_DEPTH_TEST );
 
-    auto* polygon = kvs::PolygonObject::DownCast( object );
     const size_t width = camera->windowWidth();
     const size_t height = camera->windowHeight();
     const float dpr = camera->devicePixelRatio();
@@ -47,8 +46,7 @@ void SSAOPolygonRenderer::exec( kvs::ObjectBase* object, kvs::Camera* camera, kv
     if ( BaseClass::isWindowCreated() )
     {
         BaseClass::setWindowSize( width, height );
-        BaseClass::attachObject( object );
-        BaseClass::createBufferObject( polygon );
+        BaseClass::createBufferObject( object );
         this->createShaderProgram();
         this->createFramebuffer( framebuffer_width, framebuffer_height );
     }
@@ -61,17 +59,14 @@ void SSAOPolygonRenderer::exec( kvs::ObjectBase* object, kvs::Camera* camera, kv
 
     if ( BaseClass::isObjectChanged( object ) )
     {
-        BaseClass::attachObject( object );
-        BaseClass::updateBufferObject( polygon );
+        BaseClass::updateBufferObject( object );
         this->updateShaderProgram();
         this->updateFramebuffer( framebuffer_width, framebuffer_height );
     }
 
     // Ambient occlusion.
     m_drawable.bind();
-//    m_drawable.geometryPassShader().bind();
-    BaseClass::drawBufferObject( polygon );
-//    m_drawable.geometryPassShader().unbind();
+    BaseClass::drawBufferObject( camera );
     m_drawable.unbind();
     m_drawable.draw();
 
